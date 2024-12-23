03465b989bc5dfc127e091b2b725ca6df7c35def
Diego Gettatelli <dgettatelli@gmail.com>
2024-12-23 22:59:26 +0100

    feat: add index screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,20 @@
 import { Stack } from "expo-router";
 
 export default function RootLayout() {
-  return <Stack />;
+  return (
+      <Stack
+          screenOptions={{
+            headerStyle: {
+              backgroundColor: '#4e8545',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+              fontWeight: 'bold',
+            },
+          }}
+      >
+        <Stack.Screen name={"index"} />
+        <Stack.Screen name={"details"} />
+      </Stack>
+  );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,17 @@
-import { Text, View } from "react-native";
+import {StyleSheet, Text, View} from "react-native";
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+      <View style={styles.container}>
+        <Text>Home</Text>
+      </View>
   );
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+       justifyContent: 'center',
+         alignItems: 'center',
+    },
+})
