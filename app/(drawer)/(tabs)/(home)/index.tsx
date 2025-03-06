4085ff783e737017338943d93707ce31c29bc876
Diego Gettatelli <diego.gettatelli@alfameg.com>
2025-03-06 18:15:39 +0100

    feat: edit avatar alignment in home

--- app/(drawer)/(tabs)/(home)/index.tsx
+++ app/(drawer)/(tabs)/(home)/index.tsx
@@ -6,6 +6,7 @@ import {useSharedValue, useAnimatedStyle, withTiming} from "react-native-reanima
 import Animated from "react-native-reanimated";
 import {useEffect} from "react";
 import CustomAvatar from "@/app/shared/component/custom-avatar";
+import {VStack} from "@/components/ui/vstack";
 
 const SIZE = 100.0
 
@@ -24,18 +25,31 @@ export default function Index() {
     }, [])
 
   return (
-          <Animated.View style={[reanimatedStyle, styles.container]}>
+      <VStack style={styles.header}>
+          <HStack space="md" reversed={false}>
+              <Box className="h-20 w-20 border-2 items-center justify-center">
+                  <CustomAvatar fallbackText="Umberto Race" imageUri="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
+              </Box>
+              <Box className="h-20 w-20 border-2" />
+          </HStack>
+      </VStack>
+      /*<Animated.View style={[reanimatedStyle, styles.container]}>
           <HStack space="md" reversed={false}>
               <Box className="h-20 w-20 border-amber-900 border-2 items-center justify-center">
                   <CustomAvatar fallbackText="Umberto Race" imageUri="https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80"/>
               </Box>
               <Box className="h-20 w-20 border-amber-900 border-2" />
           </HStack>
-      </Animated.View>
+      </Animated.View>*/
   );
 }
 
 const styles = StyleSheet.create({
+    header: {
+        flex: 1,
+        marginTop: '1%',
+        marginLeft: '1%'
+    },
     container: {
         flex: 1,
        justifyContent: 'center',
